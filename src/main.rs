c42c954e05089b444f41759fcb7e7c1869834a54
Fabien Geyer <fgeyer@net.in.tum.de>
2018-11-22 13:25:07 +0100

    Add actions for the selected entries

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,22 @@ extern crate regex;
 extern crate skim;
 
 use clap::{App, Arg};
-use nom_bibtex::Bibtex;
+use nom_bibtex::{Bibtex, Bibliography};
 use prettytable::{cell, format, row, Table};
 use regex::Regex;
 use skim::{Skim, SkimOptions};
+use std::collections::HashMap;
 use std::default::Default;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Cursor;
 use std::process;
+use std::process::Command;
+
+struct PBibliography<'a> {
+    bib: &'a Bibliography<'a>,
+    fields: HashMap<String, &'a String>,
+}
 
 fn read_file(filename: &str) -> String {
     let mut file = File::open(filename).unwrap();
@@ -32,6 +39,102 @@ fn strformat(val: &str) -> String {
     RE.replace_all(val, " ").to_string()
 }
 
+fn action_open_pdf(biblio: &PBibliography) {
+    if biblio.fields.contains_key("file") {
+        let fields = biblio
+            .fields
+            .get("file")
+            .unwrap()
+            .split(":")
+            .collect::<Vec<_>>();
+        if fields.len() < 3 {
+            println!("'file' field not recognized");
+            return;
+        }
+        let _res = Command::new("open").arg(fields[1]).status();
+        return;
+    }
+    println!("No PDF in entry {}", biblio.bib.citation_key());
+}
+
+fn action_open_doi(biblio: &PBibliography) {
+    if biblio.fields.contains_key("doi") {
+        let _res = Command::new("open")
+            .arg(format!(
+                "https://dx.doi.org/{}",
+                biblio.fields.get("doi").unwrap()
+            ))
+            .status();
+        return;
+    }
+    println!("No DOI in entry {}", biblio.bib.citation_key());
+}
+
+fn action_open_url(biblio: &PBibliography) {
+    if biblio.fields.contains_key("url") {
+        let _res = Command::new("open")
+            .arg(biblio.fields.get("url").unwrap())
+            .status();
+        return;
+    }
+    println!("No URL in entry {}", biblio.bib.citation_key());
+}
+
+fn action_copy_key(biblio: &PBibliography) {
+    println!("{}", biblio.bib.citation_key());
+}
+
+fn action_copy_cite(biblio: &PBibliography) {
+    println!("\\cite{{{}}}", biblio.bib.citation_key());
+}
+
+fn check_key(biblio: &PBibliography, key: &str) -> bool {
+    if key.is_empty() {
+        return true;
+    }
+    return biblio.fields.contains_key(key);
+}
+
+fn actions_menu(biblio: &PBibliography) {
+    let actions: Vec<(&str, &str, fn(&PBibliography))> = vec![
+        ("Open PDF", "file", action_open_pdf),
+        ("Open URL", "url", action_open_url),
+        ("Open DOI", "doi", action_open_doi),
+        ("Copy key", "", action_copy_key),
+        ("Copy \\cite", "", action_copy_cite),
+    ];
+    let actionsf = actions
+        .iter()
+        .filter(|x| check_key(biblio, x.1))
+        .collect::<Vec<_>>();
+    let actions_labels = actionsf.iter().map(|a| a.0).collect::<Vec<_>>();
+
+    let options: SkimOptions = SkimOptions::default().multi(false);
+    let selected_items = Skim::run_with(
+        &options,
+        Some(Box::new(Cursor::new(actions_labels.join("\n")))),
+    ).map(|out| out.selected_items)
+        .unwrap_or_else(|| Vec::new());
+
+    for item in selected_items.iter() {
+        actionsf[item.get_index()].2(biblio);
+    }
+}
+
+fn to_pbibliography<'a>(biblio: &'a Bibliography) -> PBibliography<'a> {
+    let mut map = HashMap::new();
+    for tag in biblio.tags() {
+        let (k, v) = tag;
+        let lk = k.to_ascii_lowercase();
+        map.insert(lk, v);
+    }
+
+    return PBibliography {
+        bib: biblio,
+        fields: map,
+    };
+}
+
 fn main() {
     let app = App::new("bibfzf")
         .version("18.11")
@@ -41,7 +144,8 @@ fn main() {
                 .long("key")
                 .help("Key to display from bibtex file")
                 .takes_value(true),
-        ).arg(
+        )
+        .arg(
             Arg::with_name("bibtex")
                 .help("Input bibtex file")
                 .required(true)
@@ -77,31 +181,29 @@ fn main() {
         process::exit(1);
     }
 
+    let pbibtex = bibtex
+        .bibliographies()
+        .iter()
+        .map(|x| to_pbibliography(x))
+        .collect::<Vec<_>>();
+
+    let empt = "".to_string();
+    let pfields: Vec<_> = ["title", "author", "year"]
+        .iter()
+        .map(|x| x.to_string())
+        .collect();
     let mut v: Vec<String> = Vec::new();
-    'outer: for entry in bibtex.bibliographies() {
-        let mut title: String = "".to_string();
-        let mut author: String = "".to_string();
-        let mut year: String = "".to_string();
-        for tag in entry.tags() {
-            let (k, v) = tag;
-            let lk = k.to_ascii_lowercase();
-            match lk.as_ref() {
-                "title" => {
-                    title = strformat(&v).to_string();
-                }
-                "author" => {
-                    author = strformat(&v).to_string();
-                }
-                "year" => {
-                    year = strformat(&v).to_string();
-                }
-                _ => {}
-            }
-        }
+    for entry in pbibtex.iter() {
         v.push(format!(
             "{} | {}",
-            entry.citation_key(),
-            [title, author, year].join(" - ")
+            entry.bib.citation_key(),
+            pfields
+                .iter()
+                .map(|k| {
+                    strformat(entry.fields.get(k).unwrap_or(&&empt).to_owned())
+                })
+                .collect::<Vec<_>>()
+                .join(" - ")
         ));
     }
 
@@ -111,15 +213,17 @@ fn main() {
         std::env::current_exe().unwrap().to_str().unwrap(),
         bib_path
     );
-    let options: SkimOptions = SkimOptions::default()
-        .multi(false)
-        .delimiter("|")
-        .preview(preview.as_str());
+    let options: SkimOptions = SkimOptions::default().multi(false).delimiter("|").preview(
+        preview.as_str(),
+    );
     let selected_items = Skim::run_with(&options, Some(Box::new(Cursor::new(v.join("\n")))))
         .map(|out| out.selected_items)
         .unwrap_or_else(|| Vec::new());
 
+
+    // Perform action on select entry
     for item in selected_items.iter() {
-        println!("{}", item.get_output_text());
+        let bibitem = &pbibtex[item.get_index()];
+        actions_menu(bibitem);
     }
 }
